4ce43dd90e8cc132b4275572ff8b399a01617395
Rama Andhika Pratama <137267391+kiyuzo@users.noreply.github.com>
2023-11-16 08:40:51 +0700

    Create prakpemrog4.cpp

---
+++ week_2/prakpemrog4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double num1;
+    int num2;
+
+    cout << "Please insert a positive number: ";
+    cin >> num1;
+
+    num2 = num1;
+
+    cout << "The largest integer less than or equal to the input is: " << num2 << endl;
+    return 0;
+}
